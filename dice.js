4dd9e814d44d7a96458e76d09e2140a483aa314d
Sabuja <85345738+Sabuja-GH@users.noreply.github.com>
2023-07-24 09:21:58 +0530

    Add files via upload

---
+++ dice.js
@@ -0,0 +1,14 @@
+var rand1= (Math.floor((Math.random() * 6) + 1)).toString();
+var rand2= (Math.floor((Math.random() * 6) + 1)).toString();
+document.querySelector(".img1").setAttribute("src","images/dice"+rand1+".png");
+document.querySelector(".img2").setAttribute("src","images/dice"+rand2+".png");
+
+if (rand1===rand2){
+    document.querySelector("h1").textContent="Draw!";
+}
+else if (rand1>rand2){
+    document.querySelector("h1").textContent="🚩Player 1 wins!";
+}
+else {
+    document.querySelector("h1").textContent="Player 2 wins! 🚩";
+}
