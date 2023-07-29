e541e1a73005846466ff76385f8600611cf2bb96
Sabuja <85345738+Sabuja-GH@users.noreply.github.com>
2023-07-29 20:05:58 +0530

    Update dice.js

--- dice.js
+++ dice.js
@@ -1,7 +1,7 @@
 var rand1= (Math.floor((Math.random() * 6) + 1)).toString();
 var rand2= (Math.floor((Math.random() * 6) + 1)).toString();
-document.querySelector(".img1").setAttribute("src","images/dice"+rand1+".png");
-document.querySelector(".img2").setAttribute("src","images/dice"+rand2+".png");
+document.querySelector(".img1").setAttribute("src","dice"+rand1+".png");
+document.querySelector(".img2").setAttribute("src","dice"+rand2+".png");
 
 if (rand1===rand2){
     document.querySelector("h1").textContent="Draw!";
